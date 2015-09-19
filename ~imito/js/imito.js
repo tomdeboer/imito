552e4864842d9a3fb7d062b7ff4d5c9bf2191e8f
Tom Boer <tom@tomdeboer.nl>
2015-09-19 22:07:27 +0200

    Rewrite: without jQuery

--- ~imito/js/imito.js
+++ ~imito/js/imito.js
@@ -1,66 +1,52 @@
+/*jslint  */
+var Imito = (function () {
 
+    var $container, $slides, $active_slide;
 
+    function focus($el) {
+        var index = $slides.indexOf($el);
 
-var Imito = ( function( $, undefined ){
+        $container.style.transform = "translate3d(" + ((index * -100) + '%') + ",0 , 0)";
+        $active_slide = $el;
+    }
+    function next() {
 
-	var $container, $slides, $active_slide;
+        var $next = $slides[$slides.indexOf($active_slide) + 1] || $slides[0];
 
-	$( function(){
-		
-		$container	= $('#slides_container');
-		$slides		= $container.children();
+        focus($next);
+    }
+    function previous() {
+        var $next = $slides[$slides.indexOf($active_slide) - 1] || $slides[$slides.length - 1];
 
-		$active_slide = $slides.first();
-	} );
+        focus($next);
+    }
+    function onKeyDown(e) {
+        switch (e.keyCode) {
+        case 13: // Enter
+        case 39: // Right
+        case 40: // Down
+        case 190:// Period
+        case 32: // Space
+            next();
+            break;
 
-	function next(){
+        case 8: // Backspace
+        case 37: // Left
+        case 38: // Up
+        case 188: // Comma
+            previous();
+            break;
 
-		$next = $active_slide.next();
-		if( ! $next.length ) $next = $slides.first();
+        }
+    }
 
-		focus( $next );
+    document.addEventListener('DOMContentLoaded', function () {
+        $container  = document.getElementById('slides_container');
+        $slides     = Array.prototype.slice.call($container.querySelectorAll('.slide'));
 
-		$active_slide = $next;
-	}
-	function previous(){
-		$next = $active_slide.prev();
-		if( ! $next.length ) $next = $slides.last();
+        document.addEventListener('keydown', onKeyDown);
 
-		focus( $next );
-		
-	}
+        focus($slides[0]);
+    });
 
-	function focus( $el ){
-
-		var index = $.inArray( $el[ 0 ], $slides );
-
-		$container.transition( { x: ( index * -100 ) + '%', y: 0 } );
-
-		$active_slide = $el;
-
-	};
-
-	$( this ).on('keydown', function( e ){
-
-		switch( e.keyCode ){
-			case 13: // Enter
-			case 39: // Right
-			case 40: // Down
-			case 190:// Period
-			case 32: // Space
-				next();
-			break;
-
-			case 8: // Backspace
-			case 37: // Left
-			case 38: // Up
-			case 188: // Comma
-				previous();
-			break;
-
-		}
-
-	} )
-
-
-} )( jQuery );
+}());
